25e88186de52872a04c106461546f199acd01fb9
lab2112 <brandon.coates@gmail.com>
2014-08-07 00:57:20 -0400

    bug fix

--- PortScanner.py
+++ PortScanner.py
@@ -27,14 +27,25 @@ targetIP= socket.gethostbyname(ipAddr)
 
 if scantype == 'common' or scantype=='Common':
     print "About to perform a common scan on: " + ipAddr
-    for key in common_dict:
-        print  "scanning port", common_dict[key]
-        port = common_dict[key]
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect((targetIP, port))
-        #if result == 0:
-         #   print common_dict[port]
-        sock.close()
+    try:
+        for key in common_dict:
+            print "scanning port", common_dict[key]
+            port = common_dict[key]
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #print sock.connect_ex((targetIP, port))
+            result = sock.connect((targetIP, port))
 
+            if result == 0:
+                print common_dict[port]
 
+            sock.close()
 
+    except KeyboardInterrupt:
+        print "Keyboard interrupt encountered"
+        sys.exit()
+    except socket.gaierror:
+        print "Hostname could not be resolved"
+        sys.exit()
+    except socket.error:
+        print "Couldnt connect"
+        sys.exit()
